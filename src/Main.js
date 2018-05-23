7b908c354769d9ef8f8697cd08affb8168cb9ea0
jdwhite88 <white570@purdue.edu>
2018-05-23 15:00:31 -0400

    Store notes in Main's state

--- src/Main.js
+++ src/Main.js
@@ -4,16 +4,45 @@ import Sidebar from './Sidebar';
 import NoteList from './NoteList';
 import NoteForm from './NoteForm';
 
-const Main = () => {
-    return (
-        <div
-            className="Main"
-            style={style}>
-            <Sidebar />
-            <NoteList />
-            <NoteForm />
-        </div>
-    );
+class Main extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            notes: [
+                {
+                    id: 1,
+                    title: "Prairie turnip",
+                    body: "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.",
+                    isActive: true,
+                },
+                {
+                    id: 2,
+                    title: "Dandelion cucumber",
+                    body: "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.",
+                    isActive: false,
+                },
+                {
+                    id: 3,
+                    title: "Prairie turnip",
+                    body: "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.",
+                    isActive: false,
+                },
+            ],
+        }
+    }
+    render() {
+        return (
+            <div
+                className="Main"
+                style={style}
+            >
+                <Sidebar />
+                <NoteList notes={this.state.notes}/>
+                <NoteForm />
+            </div>
+        );
+    }
 }
 
 const style = {
--- src/NoteList.js
+++ src/NoteList.js
@@ -4,30 +4,8 @@ import './NoteList.css';
 import Note from './Note';
 
 class NoteList extends React.Component {
-    constructor() {
+    constructor(notes) {
         super();
-        this.state = {
-            noteArr: [
-                {
-                    id: 1,
-                    title: "Prairie turnip",
-                    body: "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.",
-                    isActive: true,
-                },
-                {
-                    id: 2,
-                    title: "Dandelion cucumber",
-                    body: "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.",
-                    isActive: false,
-                },
-                {
-                    id: 3,
-                    title: "Prairie turnip",
-                    body: "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.",
-                    isActive: false,
-                },
-            ],
-        }
     }
     
     render() {
@@ -35,7 +13,7 @@ class NoteList extends React.Component {
             <div className="NoteList">
             <h3>Notes</h3>
             <ul id="notes">
-                {this.state.noteArr.map( (note) => {
+                {this.props.notes.map( (note) => {
                     return (
                         <a key={note.id} className={note.isActive ? "active" : ""}>
                         <li>
