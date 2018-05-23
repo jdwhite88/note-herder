8e8cfff4906f76157e28d1a0b16bb2c32ff78625
jdwhite88 <white570@purdue.edu>
2018-05-23 14:44:00 -0400

    Add ID and key to each note component

--- src/NoteList.js
+++ src/NoteList.js
@@ -9,16 +9,19 @@ class NoteList extends React.Component {
         this.state = {
             noteArr: [
                 {
+                    id: 1,
                     title: "Prairie turnip",
                     body: "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.",
                     isActive: true,
                 },
                 {
+                    id: 2,
                     title: "Dandelion cucumber",
                     body: "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.",
                     isActive: false,
                 },
                 {
+                    id: 3,
                     title: "Prairie turnip",
                     body: "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.",
                     isActive: false,
@@ -34,7 +37,7 @@ class NoteList extends React.Component {
             <ul id="notes">
                 {this.state.noteArr.map( (note) => {
                     return (
-                        <a className={note.isActive ? "active" : ""}>
+                        <a key={note.id} className={note.isActive ? "active" : ""}>
                         <li>
                             <Note title={note.title}>
                                 {note.body}
