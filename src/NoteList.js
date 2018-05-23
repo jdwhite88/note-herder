346341348e6920026c0c91b99a621ef77e1aa3dc
jdwhite88 <white570@purdue.edu>
2018-05-23 11:04:27 -0400

    Fixes title typo & renders hard-coded note list from array in state

--- src/NoteList.js
+++ src/NoteList.js
@@ -3,35 +3,50 @@ import React from 'react';
 import './NoteList.css';
 import Note from './Note';
 
-const NoteList = () => {
-    return (
-        <div className="NoteList">
-          <h3>Notes</h3>
-          <ul id="notes">
-            <a className="active">
-              <li>
-                <Note title="Prairie turnip">
-                    Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
-                </Note>
-              </li>
-            </a>
-            <a>
-              <li>
-                <Note title="Dandelion cucumber">
-                    Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
-                </Note>
-              </li>
-            </a>
-            <a>
-              <li>
-                <Note title="Prairie turnip">
-                    Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
-                </Note>
-              </li>
-            </a>
-          </ul>
-        </div>
-    );
+class NoteList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            noteArr: [
+                {
+                    title: "Prairie turnip",
+                    body: "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.",
+                    isActive: true,
+                },
+                {
+                    title: "Dandelion cucumber",
+                    body: "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.",
+                    isActive: false,
+                },
+                {
+                    title: "Prairie turnip",
+                    body: "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.",
+                    isActive: false,
+                },
+            ],
+        }
+    }
+    
+    render() {
+        return (
+            <div className="NoteList">
+            <h3>Notes</h3>
+            <ul id="notes">
+                {this.state.noteArr.map( (note) => {
+                    return (
+                        <a className={note.isActive ? "active" : ""}>
+                        <li>
+                            <Note title={note.title}>
+                                {note.body}
+                            </Note>
+                        </li>
+                        </a>
+                    );
+                })}
+            </ul>
+            </div>
+        );
+    }
 };
 
 export default NoteList;
