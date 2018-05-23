4476f62d4314e9bc75878f14288ade8bd725de66
jdwhite88 <white570@purdue.edu>
2018-05-23 10:35:10 -0400

    Refactor NoteList into Note components

---
+++ src/Note.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Note extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            title: props.title,
+            body: props.children,
+        };
+    }
+
+    render() {
+        return (
+            <div className="note">
+                <div className="note-title">
+                    {this.state.title}
+                </div>
+                <div className="note-body">
+                    <p>
+                        {this.state.body}
+                    </p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Note;
--- src/NoteList.js
+++ src/NoteList.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import './NoteList.css';
+import Note from './Note';
 
 const NoteList = () => {
     return (
@@ -8,44 +10,23 @@ const NoteList = () => {
           <ul id="notes">
             <a className="active">
               <li>
-                <div className="note">
-                  <div className="note-title">
-                    Kohlrabi welsh
-                  </div>
-                  <div className="note-body">
-                    <p>
-                      Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
-                    </p>
-                  </div>
-                </div>
+                <Note title="Prairie turnip">
+                    Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
+                </Note>
               </li>
             </a>
             <a>
               <li>
-                <div className="note">
-                  <div className="note-title">
-                    Dandelion cucumber
-                  </div>
-                  <div className="note-body">
-                    <p>
-                      Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
-                    </p>
-                  </div>
-                </div>
+                <Note title="Dandelion cucumber">
+                    Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
+                </Note>
               </li>
             </a>
             <a>
               <li>
-                <div className="note">
-                  <div className="note-title">
-                    Prairie turnip
-                  </div>
-                  <div className="note-body">
-                    <p>
-                      Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
-                    </p>
-                  </div>
-                </div>
+                <Note title="Prairie turnip">
+                    Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
+                </Note>
               </li>
             </a>
           </ul>
